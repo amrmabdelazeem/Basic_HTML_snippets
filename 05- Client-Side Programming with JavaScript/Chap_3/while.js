4da371ac4f8abfef408ef4fef58dfd58cbc6163d
Amr Elsehety <amr.abdelazeem22@gmail.com>
2022-10-09 20:34:17 +0300

    Implementing a function to accept only 3 tries for password guessing

--- 05- Client-Side Programming with JavaScript/Chap_3/while.js
+++ 05- Client-Side Programming with JavaScript/Chap_3/while.js
@@ -9,3 +9,29 @@ function getPassword(){
     }//end while
     alert("You may proceed");
 }// end get password
+
+function threeTries(){
+    //continue until user is correct
+    //or has three incorrect guesses
+    //from while.html
+    
+    var correct = "HTML5";
+    var guess = "";
+    var keepGoing = true;
+    var tries = 0;
+    
+    while(keepGoing){
+        guess = prompt("Password?");
+        if(guess== correct){
+            alert("You may proceed");
+            keepGoing= false;
+        }
+        else{
+            tries++;
+            if (tries>=3){
+                alert("Too many tries. Launching missiles...");
+                keepGoing = false;
+            }//end if
+        }//end if
+    }//end while
+}//end function
