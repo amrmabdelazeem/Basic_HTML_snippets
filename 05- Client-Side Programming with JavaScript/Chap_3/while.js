f1390277ca796e2966d805e399e5bc873ae2c800
Amr Elsehety <amr.abdelazeem22@gmail.com>
2022-10-09 20:27:04 +0300

    using while loop in guess password function

---
+++ 05- Client-Side Programming with JavaScript/Chap_3/while.js
@@ -0,0 +1,11 @@
+//from while.html
+
+function getPassword(){
+    var correct = "HTML5";
+    var guess = "";
+    
+    while(guess != correct){
+        guess = prompt("Password?");
+    }//end while
+    alert("You may proceed");
+}// end get password
