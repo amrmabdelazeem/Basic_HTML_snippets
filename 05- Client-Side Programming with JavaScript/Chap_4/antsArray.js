458e0538cf1902bfc6761e298b40bd2cb956cd3b
Amr Elsehety <amr.abdelazeem22@gmail.com>
2022-10-15 18:43:13 +0300

    Using basic functions, Array and parameters

---
+++ 05- Client-Side Programming with JavaScript/Chap_4/antsArray.js
@@ -0,0 +1,41 @@
+//from antsArray.html
+
+var distractionList = Array("", "suck his thumb", "tie his shoe",
+                            "climb a tree", "shut the door");
+
+function makeSong(){
+    var output = document.getElementById("output");
+    output.innerHTML = "";
+    for(verseNumber =1; verseNumber < distractionList.length; verseNumber++){
+        output.innerHTML += verse(verseNumber);
+        output.innerHTML += chorus(verseNumber);
+    }
+    
+}
+
+function chorus(){
+    var result = "...and they all go marching down <br/>";
+    result += "to the ground <br/>";
+    result += "to get out <br/>";
+    result += "of the rain. <br />";
+    result += "boom boom boom boom <br />";
+    result += "boom boom boom boom <br /> <br/>";
+    return result;
+}//end chorus
+
+function verse(verseNumber){
+    var distraction = distractionList[verseNumber];
+    
+    var result = "The ants go marching ";
+    result += verseNumber + " by " + verseNumber + ", ";
+    result += "hurrah, hurrah <br/>";
+    result += "The ants go marching ";
+    result += verseNumber+ " by "+ verseNumber + ", ";
+    result += "hurrah, hurrah <br/>";
+    result += "The ants go marching ";
+    result += verseNumber+ " by "+ verseNumber + "<br/>";
+    result += "The little one stops to";
+    result += distraction + "<br/> <br/>";
+    
+    return result;
+}
