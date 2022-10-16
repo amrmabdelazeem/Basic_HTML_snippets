7716037f844a87f856521a9836fb1e0ad1c47afb
Amr Elsehety <amr.abdelazeem22@gmail.com>
2022-10-16 08:58:58 +0300

    Creating a function to prompt the city choices

--- 05- Client-Side Programming with JavaScript/Chap_4/distance.js
+++ 05- Client-Side Programming with JavaScript/Chap_4/distance.js
@@ -10,3 +10,17 @@ distance = new Array(
     new Array(6476, 6760, 0, 5956),
     new Array(4000, 3470, 5956, 0)
 );
+
+//creating the function to get the two cities
+function getCity(){
+    var theCity =""; //will hold the city number
+    
+    var cityMenu = "Please choose a city by typing a number: \n";
+    cityMenu += "0) Indianapolis \n";
+    cityMenu += "1) New York \n";
+    cityMenu += "2) Tokyo \n";
+    cityMenu += "3) London \n";
+    
+    theCity = prompt(cityMenu);
+    return theCity;
+}//end getCity
