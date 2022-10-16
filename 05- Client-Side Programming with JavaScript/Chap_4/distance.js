5092fe3f456ed05dea0838dd7ba085a19d81e31c
Amr Elsehety <amr.abdelazeem22@gmail.com>
2022-10-16 08:51:59 +0300

    creating a 2-dimensional array

---
+++ 05- Client-Side Programming with JavaScript/Chap_4/distance.js
@@ -0,0 +1,12 @@
+// from distance.html
+
+//the names of the cities
+cityName = new Array["Indianapolis", "New York", "Tokyo", "London"];
+
+//creating a 2-dimension array of distances
+distance = new Array(
+    new Array(0, 648, 6476, 4000),
+    new Array(648, 0, 6760, 34700),
+    new Array(6476, 6760, 0, 5956),
+    new Array(4000, 3470, 5956, 0)
+);
