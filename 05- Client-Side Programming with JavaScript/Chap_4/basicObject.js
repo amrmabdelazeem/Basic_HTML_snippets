ebd8f1a696888940c9b5c792e5c460d210119d5d
Amr Elsehety <amr.abdelazeem22@gmail.com>
2022-10-16 09:20:27 +0300

    Bulid a baisc object

---
+++ 05- Client-Side Programming with JavaScript/Chap_4/basicObject.js
@@ -0,0 +1,11 @@
+//from basicObject.html
+
+//create the critter
+var critter = new Object();
+
+//add some properties
+critter.name = "Milo";
+critter.age = 5;
+
+//view property values
+alert("The critter's name is "+ critter.name);
