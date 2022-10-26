782db1d97c8302ea10be3b967b5c2c051c2eb24f
Amr Elsehety <amr.abdelazeem22@gmail.com>
2022-10-26 17:56:53 +0300

    building the code for programming the button to change color

---
+++ 05- Client-Side Programming with JavaScript/Chap_5/dropDownList.js
@@ -0,0 +1,9 @@
+// from dropDownList.html
+
+function ChangeColor() {
+    var selColor = document.getElementById("selColor");
+    var color = selColor.value;
+    document.body.style.backgroundColor = color;
+}
+
+//end function
