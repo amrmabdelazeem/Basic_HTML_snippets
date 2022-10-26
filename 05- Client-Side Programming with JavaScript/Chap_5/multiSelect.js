e83a6a444ffc06605a0de72b676eacb9357b2350
Amr Elsehety <amr.abdelazeem22@gmail.com>
2022-10-26 18:36:31 +0300

    building the code function with js

---
+++ 05- Client-Side Programming with JavaScript/Chap_5/multiSelect.js
@@ -0,0 +1,28 @@
+// from multiSelect.html
+
+function showChoices() {
+    // retrive data
+    var selLanguage = document.getElementById("selLanguage");
+    
+    //set up output
+    var result = "<h2>Your Languages<\/h2>";
+    result += "<ul> \n";
+    
+    //step through options
+    
+    for(i = 0; i<selLanguage.length; i++){
+        //examine current option
+        currentOption = selLanguage(i);
+        
+        //print it if it has been selected
+        if (currentOption.selected == true){
+            result += "<li>" + currentOption + "<\/li> \n";
+        }// end if
+    }//end for loop
+    
+    //finish off the list and print it out
+    result += "<\/ul> \n";
+    
+    output = document.getElementById("output");
+    output.innerHTML = result;
+}//end showChoices
