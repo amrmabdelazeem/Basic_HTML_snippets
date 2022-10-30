e14464521492365c0fa4dc89700ede6cfcdcc819
Amr Elsehety <amr.abdelazeem22@gmail.com>
2022-10-30 18:46:41 +0200

    using JS to defeat the dragon!

---
+++ 05- Client-Side Programming with JavaScript/Chap_5/radioGroup.js
@@ -0,0 +1,19 @@
+//from radioGroup.html
+
+//start function
+function fight(){
+    var weapon = document.getElementByName("weapon");
+    for (i =0; i < weapon.length; i++){
+        currentWeapon = weapon[i];
+        
+        if(currentWeapon.checked){
+            var selectedWeapon = currentWeapon.value;
+        }//end if
+    }//end for
+    var output = document.getElementById("output");
+    var response = "<h2>You defeated the dragon with a ";
+    
+    response += selectedWeapon + "</h2> \n";
+    
+    output.innerHTML = response;
+}//end function
